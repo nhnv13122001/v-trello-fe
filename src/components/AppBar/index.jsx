b567ef6485010c954e880feb0eea611e9cd73428
nhnv13122001 <nhnv13122001@gmail.com>
2024-11-01 21:38:50 +0700

    Implement basic structure of App Bar

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,6 +1,21 @@
 import Box from '@mui/material/Box'
-
+import Recent from './Menus/Recent'
+import Starred from './Menus/Starred'
 import ModeSelect from '../ModeSelect'
+import Profiles from './Menus/Profiles'
+import Badge from '@mui/material/Badge'
+import Templates from './Menus/Templates'
+import Button from '@mui/material/Button'
+import SvgIcon from '@mui/material/SvgIcon'
+import Tooltip from '@mui/material/Tooltip'
+import Workspaces from './Menus/Workspaces'
+import AppsIcon from '@mui/icons-material/Apps'
+import TextField from '@mui/material/TextField'
+import Typography from '@mui/material/Typography'
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
+
+import { ReactComponent as TrelloIcon } from '~/assets/trello.svg'
 
 function AppBar() {
     return (
@@ -8,12 +23,52 @@ function AppBar() {
             sx={{
                 display: 'flex',
                 alignItems: 'center',
+                justifyContent: 'space-between',
                 width: '100%',
                 height: (theme) => theme.trello.appBarHeight,
-                backgroundColor: 'primary.main'
+                p: 2
             }}
         >
-            <ModeSelect />
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+                <AppsIcon sx={{ color: 'primary.main' }} />
+                <Box
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        gap: 0.5,
+                        color: 'primary.main'
+                    }}
+                >
+                    <SvgIcon component={TrelloIcon} inheritViewBox />
+                    <Typography sx={{ fontSize: '1.2rem', fontWeight: 'bold' }}>
+                        Trello
+                    </Typography>
+                </Box>
+                <Workspaces />
+                <Recent />
+                <Starred />
+                <Templates />
+                <Button variant="outlined">Create</Button>
+            </Box>
+
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+                <TextField
+                    id="outlined-search"
+                    label="Search..."
+                    type="search"
+                    size="small"
+                />
+                <ModeSelect />
+                <Tooltip title="Notification">
+                    <Badge color="error" variant="dot">
+                        <NotificationsNoneIcon />
+                    </Badge>
+                </Tooltip>
+                <Tooltip title="Help">
+                    <HelpOutlineIcon />
+                </Tooltip>
+                <Profiles />
+            </Box>
         </Box>
     )
 }
