c8a9b23202c2c2ee22223ec12d01876eb9a57996
nhnv13122001 <nhnv13122001@gmail.com>
2024-11-21 19:51:12 +0700

    Implement create new column UI

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import Container from '@mui/material/Container'
 
 import BoardBar from './BoardBar/BoardBar'
+import { mockData } from '~/apis/mock-data'
 import { fetchBoardDetailsAPI } from '~/apis'
 import AppBar from '~/components/AppBar/AppBar'
 import BoardContent from './BoardContent/BoardContent'
@@ -17,8 +18,8 @@ function Board() {
   return (
     <Container disableGutters maxWidth='false' sx={{ height: '100vh' }}>
       <AppBar />
-      <BoardBar board={board} />
-      <BoardContent board={board} />
+      <BoardBar board={mockData.board} />
+      <BoardContent board={mockData.board} />
     </Container>
   )
 }
