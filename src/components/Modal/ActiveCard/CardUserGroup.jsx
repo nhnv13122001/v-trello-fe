c258a76dde76f5f1356c70010e045f0eff77f6aa
nhnv13122001 <nhnv13122001@gmail.com>
2024-12-04 22:42:06 +0700

    Complete card members feature (join, add or remove)

--- src/components/Modal/ActiveCard/CardUserGroup.jsx
+++ src/components/Modal/ActiveCard/CardUserGroup.jsx
@@ -6,12 +6,11 @@ import Tooltip from '@mui/material/Tooltip'
 import Popover from '@mui/material/Popover'
 import AddIcon from '@mui/icons-material/Add'
 import CheckCircleIcon from '@mui/icons-material/CheckCircle'
+import { useSelector } from 'react-redux'
+import { selectCurrentActiveBoard } from '~/redux/activeBoard/activeBoardSlice'
+import { CARD_MEMBER_ACTIONS } from '~/utils/constants'
 
-function CardUserGroup({ cardMemberIds = [] }) {
-  /**
-   * Xử lý Popover để ẩn hoặc hiện toàn bộ user trên một cái popup, tương tự docs để tham khảo ở đây:
-   * https://mui.com/material-ui/react-popover/
-   */
+function CardUserGroup({ cardMemberIds = [], onUpdateCardMembers }) {
   const [anchorPopoverElement, setAnchorPopoverElement] = useState(null)
   const isOpenPopover = Boolean(anchorPopoverElement)
   const popoverId = isOpenPopover ? 'card-all-users-popover' : undefined
@@ -19,22 +18,32 @@ function CardUserGroup({ cardMemberIds = [] }) {
     if (!anchorPopoverElement) setAnchorPopoverElement(event.currentTarget)
     else setAnchorPopoverElement(null)
   }
+  const board = useSelector(selectCurrentActiveBoard)
+  const FE_cardMembers = board?.FE_allUsers.filter((user) =>
+    cardMemberIds.includes(user._id)
+  )
 
-  // Lưu ý ở đây chúng ta không dùng Component AvatarGroup của MUI bởi nó không hỗ trợ tốt trong việc chúng ta cần custom & trigger xử lý phần tử tính toán cuối, đơn giản là cứ dùng Box và CSS - Style đám Avatar cho chuẩn kết hợp tính toán một chút thôi.
+  const handleUpdateCardMembers = (user) => {
+    const incomingMemberInfo = {
+      userId: user._id,
+      action: cardMemberIds.includes(user._id)
+        ? CARD_MEMBER_ACTIONS.REMOVE
+        : CARD_MEMBER_ACTIONS.ADD
+    }
+    onUpdateCardMembers(incomingMemberInfo)
+  }
   return (
     <Box sx={{ display: 'flex', gap: '4px', flexWrap: 'wrap' }}>
-      {/* Hiển thị các user là thành viên của card */}
-      {[...Array(8)].map((_, index) => (
-        <Tooltip title='trungquandev' key={index}>
+      {FE_cardMembers.map((user, index) => (
+        <Tooltip title={user?.displayName} key={index}>
           <Avatar
             sx={{ width: 34, height: 34, cursor: 'pointer' }}
-            alt='trungquandev'
-            src='https://trungquandev.com/wp-content/uploads/2019/06/trungquandev-cat-avatar.png'
+            alt={user?.displayName}
+            src={user?.avatar}
           />
         </Tooltip>
       ))}
 
-      {/* Nút này để mở popover thêm member */}
       <Tooltip title='Add new member'>
         <Box
           aria-describedby={popoverId}
@@ -67,7 +76,6 @@ function CardUserGroup({ cardMemberIds = [] }) {
         </Box>
       </Tooltip>
 
-      {/* Khi Click vào + ở trên thì sẽ mở popover hiện toàn bộ users trong board để người dùng Click chọn thêm vào card  */}
       <Popover
         id={popoverId}
         open={isOpenPopover}
@@ -84,21 +92,28 @@ function CardUserGroup({ cardMemberIds = [] }) {
             gap: 1.5
           }}
         >
-          {[...Array(16)].map((_, index) => (
-            <Tooltip title='trungquandev' key={index}>
-              {/* Cách làm Avatar kèm badge icon: https://mui.com/material-ui/react-avatar/#with-badge */}
+          {board?.FE_allUsers.map((user, index) => (
+            <Tooltip title={user?.displayName} key={index}>
               <Badge
                 sx={{ cursor: 'pointer' }}
                 overlap='rectangular'
                 anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
                 badgeContent={
-                  <CheckCircleIcon fontSize='small' sx={{ color: '#27ae60' }} />
+                  cardMemberIds.includes(user._id) && (
+                    <CheckCircleIcon
+                      fontSize='small'
+                      sx={{ color: '#27ae60' }}
+                    />
+                  )
                 }
+                onClick={() => {
+                  handleUpdateCardMembers(user)
+                }}
               >
                 <Avatar
                   sx={{ width: 34, height: 34 }}
-                  alt='trungquandev'
-                  src='https://trungquandev.com/wp-content/uploads/2019/06/trungquandev-cat-avatar.png'
+                  alt={user?.displayName}
+                  src={user?.avatar}
                 />
               </Badge>
             </Tooltip>
